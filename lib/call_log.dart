e5afae87c80ba3cf2bb4832167e6c7f72af5ec20
Martin Hlavňa <martin.hlavna@4q.sk>
2021-09-30 17:26:01 +0200

    Added new DateTime parameters as an alternative to ints

--- lib/call_log.dart
+++ lib/call_log.dart
@@ -4,8 +4,7 @@ import 'package:flutter/services.dart';
 
 /// main call_log plugin class
 class CallLog {
-  static const Iterable<CallLogEntry> _EMPTY_RESULT =
-      Iterable<CallLogEntry>.empty();
+  static const Iterable<CallLogEntry> _EMPTY_RESULT = Iterable<CallLogEntry>.empty();
   static const MethodChannel _channel = MethodChannel('sk.fourq.call_log');
 
   /// Get all call history log entries. Permissions are handled automatically
@@ -15,8 +14,10 @@ class CallLog {
   }
 
   /// Query call history log entries
-  /// dateFrom: unix timestamp. precision in millis
-  /// dateTo: unix timestamp. precision in millis
+  /// dateFrom: unix timestamp. precision in millis. Use only one of dateFrom dateTimeFrom
+  /// dateTo: unix timestamp. precision in millis. Use only one of dateTo dateTimeTo
+  /// dateTimeFrom: DateTime. Same as dateFrom, but accepting DateTime. Use only one of dateFrom dateTimeFrom
+  /// dateTimeTo: DateTime. Same as dateTo, but accepting DateTime. Use only one of dateTo dateTimeTo
   /// durationFrom: minimal call length in seconds
   /// durationTo: minimal call length in seconds
   /// name: call participant name (present only if in contacts)
@@ -27,6 +28,8 @@ class CallLog {
     int? dateTo,
     int? durationFrom,
     int? durationTo,
+    DateTime? dateTimeFrom,
+    DateTime? dateTimeTo,
     String? name,
     String? number,
     CallType? type,
@@ -37,9 +40,20 @@ class CallLog {
     String? cachedMatchedNumber,
     String? phoneAccountId,
   }) async {
+    assert(!(dateFrom != null && dateTimeFrom != null), 'use only one of dateTimeFrom/dateFrom');
+    assert(!(dateTo != null && dateTimeTo != null), 'use only one of dateTimeTo/dateTo');
+
+    //NOTE: Since we are accepting date params both as timestamps and DateTime objects
+    // we need to determine which one to use
+    int? _dateFrom = dateFrom;
+    _dateFrom ??= dateTimeFrom?.millisecondsSinceEpoch;
+
+    int? _dateTo = dateTo;
+    _dateTo ??= dateTimeTo?.millisecondsSinceEpoch;
+
     final Map<String, String?> params = <String, String?>{
-      'dateFrom': dateFrom?.toString(),
-      'dateTo': dateTo?.toString(),
+      'dateFrom': _dateFrom?.toString(),
+      'dateTo': _dateTo?.toString(),
       'durationFrom': durationFrom?.toString(),
       'durationTo': durationTo?.toString(),
       'name': name,
@@ -50,16 +64,13 @@ class CallLog {
       'cachedMatchedNumber': cachedMatchedNumber,
       'phoneAccountId': phoneAccountId,
     };
-    final Iterable<dynamic>? records =
-        await _channel.invokeMethod('query', params);
-    return records?.map((dynamic m) => CallLogEntry.fromMap(m)) ??
-        _EMPTY_RESULT;
+    final Iterable<dynamic>? records = await _channel.invokeMethod('query', params);
+    return records?.map((dynamic m) => CallLogEntry.fromMap(m)) ?? _EMPTY_RESULT;
   }
 }
 
 ///method for returning the callType
 CallType getCallType(int n) {
-  
   if (n == 100) {
     //return the wifi outgoing call
     return CallType.wifiOutgoing;
